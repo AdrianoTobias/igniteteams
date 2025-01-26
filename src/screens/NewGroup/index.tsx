5c32fe67f4a2c08ca7041941ae5307ea5c1a771e
Adriano <adrianoact@msn.com>
2025-01-26 18:19:34 -0300

    CSS-in-JS: create NewGroup screen

---
+++ src/screens/NewGroup/index.tsx
@@ -0,0 +1,26 @@
+import { Container, Content, Icon } from "./styles";
+
+import { Header } from "@components/Header";
+import { Button } from "@components/Button";
+import { Highlight } from "@components/Highlight";
+
+export function NewGroup() {
+  return (
+    <Container>
+      <Header showBackButton />
+
+      <Content>
+        <Icon />
+
+        <Highlight 
+          title="Nova turma"
+          subtitle="crie uma turma para adicionar pessoas"
+        />
+
+        <Button 
+          title="Criar"
+        />
+      </Content>
+    </Container>
+  )
+}
