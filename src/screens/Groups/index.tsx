31fa4767bd2c942ad2401bb00f0a4b086d83b13f
Adriano <adrianoact@msn.com>
2025-01-26 18:06:37 -0300

    CSS-in-JS: Button component

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,9 +4,10 @@ import { FlatList } from 'react-native';
 import { GroupCard } from '@components/GroupCard';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
+import { ListEmpty } from '@components/ListEmpty';
+import { Button } from '@components/Button';
 
 import { Container } from './styles';
-import { ListEmpty } from '@components/ListEmpty';
 
 export function Groups() {
   const [groups, setGroups] = useState<string[]>([]);
@@ -19,19 +20,23 @@ export function Groups() {
         subtitle="jogue com a sua turma"
       />
 
-    <FlatList 
-      data={groups}
-      keyExtractor={item => item}
-      renderItem={({ item }) => (
-        <GroupCard 
-          title={item} 
-        />
-      )}
-      contentContainerStyle={groups.length === 0 && { flex: 1 }}
-      ListEmptyComponent={() => (
-        <ListEmpty message="Que tal cadastrar a primeira turma?" />
-      )}
-    />
+      <FlatList 
+        data={groups}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <GroupCard 
+            title={item} 
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && { flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
+
+      <Button 
+        title='Criar nova turma'
+      />
     </Container>
   );
 }
